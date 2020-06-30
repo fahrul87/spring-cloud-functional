761a9bc0ba379524ba851bca791c17231044913f
Fahrul Arya Sugita <faryasugita@gmail.com>
2020-06-30 13:58:52 +0700

    istimiwir

---
+++ src/main/java/com/fahrul/springcloudfunctional/SpringCloudFunctionalApplication.java
@@ -0,0 +1,37 @@
+package com.fahrul.springcloudfunctional;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+
+@SpringBootApplication
+public class SpringCloudFunctionalApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFunctionalApplication.class, args);
+	}
+	
+	@Bean
+	public Function<String, String> reverse() {
+		return (input) -> new StringBuilder(input).reverse().toString();
+	}
+
+	@Bean
+	public Supplier<Book> getBook() {
+		return () -> new Book(101, "Core Java");
+	}
+
+	@Bean
+	public Consumer<String> printMessage() {
+		return (input) -> System.out.println(input);
+	}
+	
+
+}
---
+++ src/test/java/com/fahrul/springcloudfunctional/SpringCloudFunctionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahrul.springcloudfunctional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudFunctionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
